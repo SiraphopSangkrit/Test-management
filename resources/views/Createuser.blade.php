dcd9ddf144d0b320d4d90a02c38402eced06be9f
SiraphopSangkrit <Siraphop_sangkrit@kkumail.com>
2022-03-26 00:40:00 +0700

    Update Createuser.blade.php

--- resources/views/Createuser.blade.php
+++ resources/views/Createuser.blade.php
@@ -54,7 +54,7 @@
                             </div>
                         </div>
 
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " type="submit">
                             Create User
                         </button>
 
