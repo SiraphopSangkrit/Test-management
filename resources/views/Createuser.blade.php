25af973678c258317e8dfe39b82eab5da09c586a
SiraphopSangkrit <Siraphop_sangkrit@kkumail.com>
2022-03-26 23:07:18 +0700

    Update Login page

--- resources/views/Createuser.blade.php
+++ resources/views/Createuser.blade.php
@@ -54,6 +54,8 @@
                             </div>
                         </div>
 
+                        
+
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " type="submit" for="role_id" value="{{ __('Register as:teacher') }}">
                             Create User
                         </button>
